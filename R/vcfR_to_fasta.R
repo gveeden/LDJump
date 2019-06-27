150c0032b170fbf11362a2cdceb23b3b13615e1e
gveeden <gveeden@gmail.com>
2019-06-27 12:06:15 +0200

    Updated vcfR_to_fasta.R to add ID's to SNPs with no rsID

--- R/vcfR_to_fasta.R
+++ R/vcfR_to_fasta.R
@@ -1,5 +1,6 @@
 vcfR_to_fasta = function(seqName, refName = NULL, ext.ind = T, cons = F, ext.haps = T, start = NULL) {
-  pop = vcfR::read.vcfR(file = seqName, limit = 1e+07, nrows = -1, skip = 0, cols = NULL, convertNA = F, verbose = T)
+  input = vcfR::read.vcfR(file = seqName, limit = 1e+07, nrows = -1, skip = 0, cols = NULL, convertNA = F, verbose = T)
+  pop = vcfR::addID(input, sep="_")
   ref = ape::read.dna(file = refName, format = "fasta")
   pop.dnabin = vcfR::vcfR2DNAbin(pop, extract.indels = ext.ind, consensus = cons, extract.haps = ext.haps, ref.seq = ref, start.pos = start, verbose = TRUE)
   ### with consensus = T we have 1 sequence per individual ####
